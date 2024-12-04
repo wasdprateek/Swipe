b1e8c19e778a00f29e3c369309d9122d4aa21cc6
Prateek <raiprateek00@gmail.com>
2024-12-05 00:59:29 +0530

    Initial Commit

---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by Prateek Kumar Rai on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwipeTests/SwipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeTests.swift
+//  SwipeTests
+//
+//  Created by Prateek Kumar Rai on 05/12/24.
+//
+
+import Testing
+@testable import Swipe
+
+struct SwipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
